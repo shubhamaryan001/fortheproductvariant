bddf7a21613086d746f425a9d6739ded14cb4496
Shubham <saurabharyan30@gmail.com>
2020-01-01 17:16:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     return (
       <div>
         {this.props.isMobile ? (
-          <MobileRoutes />
+          <Routes />
         ) : (
           <>
             <Routes />
